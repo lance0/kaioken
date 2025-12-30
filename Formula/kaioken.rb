1515ad666b59ae87de4a6f9708c695b18f9c1cb8
Lance Tuller <lancey3@gmail.com>
2025-12-30 08:39:51 -0500

    chore: update Homebrew formula with v1.1.1 checksums

--- Formula/kaioken.rb
+++ Formula/kaioken.rb
@@ -11,20 +11,20 @@ class Kaioken < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/lance0/kaioken/releases/download/v#{version}/kaioken-macos-aarch64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_MACOS_ARM64"
+      sha256 "d87cdb91c3e377545a776c94aa9f2831fe62c7a055c1c3ddb2e98c61eb521cca"
     else
       url "https://github.com/lance0/kaioken/releases/download/v#{version}/kaioken-macos-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_MACOS_X86_64"
+      sha256 "b2b27a4fcc1dad5b5f43d182f1afa4cf6198a8cdaf78f7c9a0c8ab8297ab4102"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/lance0/kaioken/releases/download/v#{version}/kaioken-linux-aarch64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_LINUX_ARM64"
+      sha256 "5913ea9952c80a64b2e4b41dfb789e516406938d35c8f6ad8beb9d7f18728210"
     else
       url "https://github.com/lance0/kaioken/releases/download/v#{version}/kaioken-linux-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_LINUX_X86_64"
+      sha256 "b27300f4c5254683ec5edbb8553b6b85f75a341497abde45b329215afdfb2c3b"
     end
   end
 
